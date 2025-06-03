ce3752ae0eddd5f05b4d8e4da991d5a9f359e90d
MOHAMMAD FAIZAN ASHRAF <134165364+ProgrammingTechniques@users.noreply.github.com>
2025-06-03 15:10:24 +0530

    Email and WhatsApp Campaign

--- campaigns/urls.py
+++ campaigns/urls.py
@@ -1,3 +1,11 @@
 from django.urls import path
+from .views import (
+    EmailCampaignView,
+    WhatsAppTemplateSendView,
+    redirect_root_to_campaign,
+)
 
-urlpatterns = []
+urlpatterns = [
+    path("send-campaign/", EmailCampaignView.as_view(), name="send_campaign"),
+    path("send-whatsapp/", WhatsAppTemplateSendView.as_view(), name="send_whatsapp"),
+]
